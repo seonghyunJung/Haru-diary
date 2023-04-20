3858aaab13f9d6a2aa1c632a97426c94ec27e8f5
seonghyunJung <tjdgusus@gmail.com>
2023-04-20 19:36:06 +0900

    Feat: Fullcalendar 라이브러리 연동 및 달력에서 일기 조회 및 수정, 삭제 기능 구현 완료 - close #5

--- haru/urls.py
+++ haru/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("diary/modify/<int:diary_id>/", views.diary_modify, name="diary_modify"),
     path("diary/delete/<int:diary_id>/", views.diary_delete, name="diary_delete"),
     path("calendar/", views.calendar_page, name="calendar_page"),
+    path("all_diaries/", views.all_diaries, name="all_diaries"),
     path("statistics/", views.graph_page, name="graph_page"),
 ]
--- haru/views.py
+++ haru/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
@@ -7,27 +8,30 @@ from haru.forms import DiaryForm
 from .models import Diary
 
 
+@login_required(login_url="account:login")
 def index(request):
     try:
-        diary = Diary.objects.get(create_date=timezone.now().date())
+        diary = Diary.objects.get(
+            author_id=request.user.id, create_date=timezone.now().date()
+        )
         return redirect("haru:diary_modify", diary_id=diary.id)
     except:
-        return redirect("haru:diary_create")
+        return redirect("haru:diary_create", date=timezone.now().date())
 
 
 @login_required(login_url="account:login")
-def diary_create(request):
+def diary_create(request, date):
     if request.method == "POST":
         form = DiaryForm(request.POST)
         if form.is_valid():
             diary = form.save(commit=False)
             diary.author = request.user
-            diary.create_date = timezone.now().date()
+            diary.create_date = date
             diary.save()
-            return redirect("haru:index")
+            # return redirect("haru:index")
     else:
         form = DiaryForm()
-    context = {"form": form, "date": timezone.now().date()}
+    context = {"form": form, "date": date}
     return render(request, "haru/diary_form.html", context)
 
 
@@ -39,7 +43,6 @@ def diary_modify(request, diary_id):
         if form.is_valid():
             diary = form.save(commit=False)
             diary.save()
-            return redirect("haru:index")
     else:
         form = DiaryForm(instance=diary)
     context = {"form": form, "diary": diary, "date": diary.create_date}
@@ -50,11 +53,30 @@ def diary_modify(request, diary_id):
 def diary_delete(request, diary_id):
     diary = get_object_or_404(Diary, pk=diary_id)
     diary.delete()
-    return redirect("haru:index")
+    return redirect("haru:calendar_page")
 
 
+@login_required(login_url="account:login")
 def calendar_page(request):
-    return render(request, "haru/calendar.html", {})
+    all_diaries = Diary.objects.filter(author_id=request.user.id)
+    context = {"diaries": all_diaries}
+    return render(request, "haru/fullcalendar.html", context)
+
+
+@login_required(login_url="account:login")
+def all_diaries(request):
+    all_diaries = Diary.objects.filter(author_id=request.user.id)
+    out = []
+    for diary in all_diaries:
+        out.append(
+            {
+                "title": diary.content,
+                "id": diary.id,
+                "date": diary.create_date,
+            }
+        )
+
+    return JsonResponse(out, safe=False)
 
 
 def graph_page(request):
